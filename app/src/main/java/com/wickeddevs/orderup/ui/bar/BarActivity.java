751d54470ee7058713d338eac96b08a96f09bc31
HickeyMW <hickey.mw@gmail.com>
2018-11-03 10:46:17 -0500

    BarActivity

---
+++ app/src/main/java/com/wickeddevs/orderup/ui/bar/BarActivity.java
@@ -0,0 +1,15 @@
+package com.wickeddevs.orderup.ui.bar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wickeddevs.orderup.R;
+
+public class BarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar);
+    }
+}
