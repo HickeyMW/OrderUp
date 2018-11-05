7a9d5560d0a3ab1aa77512419df307965370186b
HickeyMW <hickey.mw@gmail.com>
2018-11-05 12:51:58 -0600

    Update app/src/main/java/com/wickeddevs/orderup/data/Appetizer.java

---
+++ app/src/main/java/com/wickeddevs/orderup/data/Appetizer.java
@@ -0,0 +1,14 @@
+package com.wickeddevs.orderup.data;
+
+public class Appetizer extends Item {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
